58ab15bee87ede2bea7890cc8c5df47b5a730bd2
Prasanth <prasanthcena968@gmail.com>
2023-01-12 10:46:57 +0530

    README file added

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# MVVM Retrofit
+
+1, What is the MVVM
+MVVM is a software design pattern used by programmers to develop user interfaces for web applications. It makes it easy to maintain and develop complicated user interfaces. The viewmodel in MVVM is efficient in managing data and business logic
+
+2, What is the MVVM Android]
+MVVM stands for Model, View, ViewModel. Model: This holds the data of the application. It cannot directly talk to the View. Generally, it's recommended to expose the data to the ViewModel through Observables. View: It represents the UI of the application devoid of any Application Logic.
+
+3, What is the ViewModelObserver
+
+Attach the Observer object to the LiveData object using the observe() method. The observe() method takes a LifecycleOwner object. This subscribes the Observer object to the LiveData object so that it is notified of changes. You usually attach the Observer object in a UI controller, such as an activity or fragment.
+
--- app/src/main/java/com/skyblue/mvvmretrofitactivity/MainActivity.java
+++ app/src/main/java/com/skyblue/mvvmretrofitactivity/MainActivity.java
@@ -33,16 +33,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        // responsible for measuring and positioning item,
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
 
+        // adding array list to our adapter class.
         adapter = new UserListAdapter(userlist);
+
+        // setting adapter to our recycler view.
         binding.recyclerView.setAdapter(adapter);
 
+        // framework provides the ViewModels, a special mechanism is required to create instances of them
         listViewModel = new ViewModelProvider(this).get(UserListViewModel.class);
+
+        /*
+         Attach the Observer object to the LiveData object using the observe() method. The observe() method takes a LifecycleOwner object.
+         This subscribes the Observer object to the LiveData object so that it is notified of changes. You usually attach the Observer object
+         in a UI controller, such as an activity or fragment.
+         */
         listViewModel.getUserListObserver().observe(this, new Observer<List<UserModel>>() {
             @Override
             public void onChanged(List<UserModel> list) {
+                // check user array list empty or null
                 if (list!=null){
                     userlist = list;
                     adapter.updateUserList(list);
@@ -54,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        // call retrofit api
         listViewModel.makeApiCall();
     }
 }
